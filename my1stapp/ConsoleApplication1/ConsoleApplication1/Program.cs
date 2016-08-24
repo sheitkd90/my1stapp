21d2004a3bb1b9ddd1b6035f361a37fde0d72893
Sheila Angeles <Sheila Angeles>
2016-08-24 12:58:20 -0500

    Hello World!

---
+++ my1stapp/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,22 @@
+﻿//A Hello World! program in C#.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Hello
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+
+            //Keep the console window open in debub mode.
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
