cfde4277419e42389044a40a75900d1850fd94da
Sheila Angeles <Sheila Angeles>
2016-08-25 12:58:35 -0500

    Sum of two numbers, first try

---
+++ my1stapp/Sum2numb/Sum2numb/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sum2numb
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Declaration of variables
+            int a, b;
+            
+         Console.WriteLine("\n Enter the first number: ");
+          Program:
+            a = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("\n Enter the second number: ");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            int sum = a + b;
+
+        Console.WriteLine("\n The sum of the 2 numbers is: {0} ", sum);
+        
+        Console.WriteLine("\n To continue type the first number to exit type Quit  ");
+            string quit = System.Console.ReadLine();
+        }
+    }
+}
--- my1stapp/Sum2numb/Sum2numb/Properties/AssemblyInfo.cs
+++ my1stapp/Sum2numb/Sum2numb/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("my1stapp")]
+[assembly: AssemblyTitle("Sum2numb")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("my1stapp")]
+[assembly: AssemblyProduct("Sum2numb")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("f0937eb9-e66d-4b7f-b03e-6f9feccc9a3c")]
+[assembly: Guid("e54f9cee-0f2f-4fdf-9b3d-9f24ebda8e21")]
 
 // Version information for an assembly consists of the following four values:
 //
